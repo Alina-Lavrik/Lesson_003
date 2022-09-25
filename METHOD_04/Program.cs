fade1b1776e79030c0ea7193f3a1b3479731f966
Alina Lavrik <alina_voro@mail.ru>
2022-09-25 22:25:58 +0300

    Add Method 04_таблица умножения

---
+++ METHOD_04/Program.cs
@@ -0,0 +1,24 @@
+﻿
+/*
+string Method4(int count, string text)
+{
+    string result = string.Empty;
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, "aal");
+Console.WriteLine(res); */
+
+// Цикл в цикле, пример с таблицей умножения
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
